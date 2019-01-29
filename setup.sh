f99db4e17bdb09aa6e891eaa3757360fa8c29042
ryotaro_koike <ryotaro.koike@okadanet.org>
2019-01-30 02:05:06 +0900

    first commit

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | \
+  sudo apt-key add -
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | \
+  sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+
+sudo apt-get update
+sudo apt-get -y install docker-ce
+sudo adduser `whoami` docker
+sudo apt-get install -y nvidia-docker2
+sudo pkill -SIGHUP dockerd
+
